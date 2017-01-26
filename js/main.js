4e34bb3119eaadc9961bd8fc12cf1ae8678bd6a9
Brian Norris <bnorris@marlinco.com>
2017-01-26 11:37:54 -0600

    Loading Vue, trying XHR to fortune cookie

--- js/main.js
+++ js/main.js
@@ -1,1 +1,26 @@
 
+var app = new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello Vue!'
+  }
+})
+
+
+var data = null;
+
+var xhr = new XMLHttpRequest();
+xhr.withCredentials = true;
+
+xhr.addEventListener("readystatechange", function () {
+  if (this.readyState === 4) {
+    console.log(this.responseText);
+  }
+});
+
+xhr.open("GET", "http://fortunecookieapi.herokuapp.com/v1/fortunes?limit=1&skip=540");
+xhr.setRequestHeader("cache-control", "no-cache");
+xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
+xhr.setRequestHeader("postman-token", "c2a8ef06-1ab0-919f-1f22-70dcbb5e0846");
+
+xhr.send(data);
