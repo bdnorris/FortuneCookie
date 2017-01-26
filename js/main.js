5caafd4f47d2ed27a0e47dafcbb1f99120e243d5
Brian Norris <bnorris@marlinco.com>
2017-01-26 13:51:18 -0600

    adding my own json from
    http://fortunecookieapi.herokuapp.com/

--- js/main.js
+++ js/main.js
@@ -1,16 +1,8 @@
-
-var app = new Vue({
-  el: '#app',
-  data: {
-    message: 'Hello Vue!'
-  }
-})
-
-
 var data = null;
 
 var xhr = new XMLHttpRequest();
 xhr.withCredentials = true;
+//xhr.responseType = 'json';
 
 xhr.addEventListener("readystatechange", function () {
   if (this.readyState === 4) {
@@ -18,9 +10,17 @@ xhr.addEventListener("readystatechange", function () {
   }
 });
 
-xhr.open("GET", "http://fortunecookieapi.herokuapp.com/v1/fortunes?limit=1&skip=540");
+xhr.open("GET", "http://localhost:3000/js/proverbs.json");
 xhr.setRequestHeader("cache-control", "no-cache");
-xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
-xhr.setRequestHeader("postman-token", "c2a8ef06-1ab0-919f-1f22-70dcbb5e0846");
+xhr.setRequestHeader("postman-token", "67638480-5a7f-5ff2-821f-ca3a98a18388");
+xhr.setRequestHeader("responseType", "json");
 
 xhr.send(data);
+
+
+var app = new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello Vue!'
+  }
+})
